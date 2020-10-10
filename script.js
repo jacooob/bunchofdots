e53ca61dc9b69e9b240a3eeeac57c6836d8a503f
Jacob Allen <jahocob@gmail.com>
2020-10-10 16:24:40 -0700

    eh

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var wX = window.innerWidth;
 var wY = window.innerHeight;
 
+const bg = document.querySelector('#bg');
+
 const boxColor = 'red';
 const boxWidth = 5;
 const boxHeight = 5;
@@ -11,7 +13,7 @@ const boxMargin = 1;
 
 var styles = document.createElement('style');
 styles.innerHTML = '.myBox {background-color:'+boxColor+';width:'+boxWidth+'px;height:'+boxHeight+'px;padding:'+boxPadding+'px;border:'+borderColor+' solid '+boxBorder+'px;margin:'+boxMargin+'px;display:inline-block;}';
-document.body.appendChild(styles); 
+document.body.appendChild(styles);
 
 var w = boxWidth + (boxPadding * 2) + (boxBorder * 2) + (boxMargin * 2);
 
@@ -27,9 +29,9 @@ var howManyBoxes = x * (y-1);
 console.log(howManyBoxes);
 
 for(i=0;i<howManyBoxes;i++) {
-  var myBox = document.createElement('div');  
+  var myBox = document.createElement('div');
   myBox.setAttribute('class','myBox');
   myBox.setAttribute('id','box'+i  )
-  document.body.appendChild(myBox);
+  bg.appendChild(myBox);
 
 }
