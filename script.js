f4adf93105ba74df10710fff52c166f695ff090d
Jacob Allen <jahocob@gmail.com>
2020-10-10 18:29:51 -0700

    eh

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const bg = document.querySelector('#bg');
 
-$('#navigation').draggable();
+$('#navigation').draggable({
+  containment: 'document',
+});
+
+$(document).ready(function() {
+  setTimeout(function() {
+    $('#navigation').toggle('slide',500);
+  }, 1000);
+});
+
+function openPage(v) {
+  $('#page'+v).toggle('slide',500);
+}
