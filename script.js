6633cebe5273065994eeea0627dd05104f25c5f4
Jacob Allen <jahocob@gmail.com>
2020-10-11 02:12:08 +0000

    remove jquery

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 const bg = document.querySelector('#bg');
 
-$('#navigation').draggable({
-  containment: 'document',
-});
-
-$(document).ready(function() {
-  setTimeout(function() {
-    $('#navigation').toggle('slide',500);
-  }, 1000);
-});
-
 function openPage(v) {
-  $('#page'+v).toggle('slide',500);
+  console.log(v)
 }
